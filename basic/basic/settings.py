a2e69862989ff25808fd2d72f722ee291f71cdf2
ECallihan <edward.callihan@gmail.com>
2021-11-25 02:12:59 -0500

    deleted static files import

--- basic/basic/settings.py
+++ basic/basic/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
-from edsplayground.client.client.settings import STATICFILES_DIRS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
